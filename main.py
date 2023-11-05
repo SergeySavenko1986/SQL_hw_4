35964854838c6eac6cf7f8345b76bdccf530fe7f
sesavi test <sesavi@mail.ru>
2023-11-05 18:29:40 +0300

    Update_1 commit

--- main.py
+++ main.py
@@ -1,28 +1,207 @@
 import psycopg2
-
-def create_db(conn):
-    pass
-
-def add_client(conn, first_name, last_name, email, phones=None):
-    pass
-
-def add_phone(conn, client_id, phone):
-    pass
-
-def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
-    pass
-
-def delete_phone(conn, client_id, phone):
-    pass
-
-def delete_client(conn, client_id):
-    pass
-
-def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
-    pass
+from pprint import pprint
 
 
-with psycopg2.connect(database="clients_db", user="postgres", password="postgres") as conn:
-    pass  # вызывайте функции здесь
+def create_db(cur):
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS clients(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        lastname VARCHAR(30),
+        email VARCHAR(254)
+        );
+    """)
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS phonenumbers(
+        number VARCHAR(11) PRIMARY KEY,
+        client_id INTEGER REFERENCES clients(id)
+        );
+    """)
+    return
 
-conn.close()
+
+def delete_db(cur):
+    cur.execute("""
+        DROP TABLE clients, phonenumbers CASCADE;
+        """)
+
+
+def insert_tel(cur, client_id, tel):
+    cur.execute("""
+        INSERT INTO phonenumbers(number, client_id)
+        VALUES (%s, %s)
+        """, (tel, client_id))
+    return client_id
+
+
+def insert_client(cur, name=None, surname=None, email=None, tel=None):
+    cur.execute("""
+        INSERT INTO clients(name, lastname, email)
+        VALUES (%s, %s, %s)
+        """, (name, surname, email))
+    cur.execute("""
+        SELECT id from clients
+        ORDER BY id DESC
+        LIMIT 1
+        """)
+    id = cur.fetchone()[0]
+    if tel is None:
+        return id
+    else:
+        insert_tel(cur, id, tel)
+        return id
+
+
+def update_client(cur, id, name=None, surname=None, email=None):
+    cur.execute("""
+        SELECT * from clients
+        WHERE id = %s
+        """, (id, ))
+    info = cur.fetchone()
+    if name is None:
+        name = info[1]
+    if surname is None:
+        surname = info[2]
+    if email is None:
+        email = info[3]
+    cur.execute("""
+        UPDATE clients
+        SET name = %s, lastname = %s, email =%s 
+        where id = %s
+        """, (name, surname, email, id))
+    return id
+
+
+def delete_phone(cur, number):
+    cur.execute("""
+        DELETE FROM phonenumbers 
+        WHERE number = %s
+        """, (number, ))
+    return number
+
+
+def delete_client(cur, id):
+    cur.execute("""
+        DELETE FROM phonenumbers
+        WHERE client_id = %s
+        """, (id, ))
+    cur.execute("""
+        DELETE FROM clients 
+        WHERE id = %s
+       """, (id,))
+    return id
+
+
+def find_client(cur, name=None, surname=None, email=None, tel=None):
+    if name is None:
+        name = '%'
+    else:
+        name = '%' + name + '%'
+    if surname is None:
+        surname = '%'
+    else:
+        surname = '%' + surname + '%'
+    if email is None:
+        email = '%'
+    else:
+        email = '%' + email + '%'
+    if tel is None:
+        cur.execute("""
+            SELECT c.id, c.name, c.lastname, c.email, p.number FROM clients c
+            LEFT JOIN phonenumbers p ON c.id = p.client_id
+            WHERE c.name LIKE %s AND c.lastname LIKE %s
+            AND c.email LIKE %s
+            """, (name, surname, email))
+    else:
+        cur.execute("""
+            SELECT c.id, c.name, c.lastname, c.email, p.number FROM clients c
+            LEFT JOIN phonenumbers p ON c.id = p.client_id
+            WHERE c.name LIKE %s AND c.lastname LIKE %s
+            AND c.email LIKE %s AND p.number like %s
+            """, (name, surname, email, tel))
+    return cur.fetchall()
+
+
+if __name__ == '__main__':
+    with psycopg2.connect(database="1", user="postgres",
+                          password="123456") as conn:
+        with conn.cursor() as curs:
+            # Удаление таблиц перед запуском
+            delete_db(curs)
+            # 1. Cоздание таблиц
+            create_db(curs)
+            print("БД создана")
+            # 2. Добавляем 5 клиентов
+            print("Добавлен клиент id: ",
+                  insert_client(curs, "Иван", "Иванов", "ivanov@mail.ru"))
+            print("Добавлен клиент id: ",
+                  insert_client(curs, "Петр", "Петров",
+                                "petrov@mail.ru", 79993318644))
+            print("Добавлен клиент id: ",
+                  insert_client(curs, "Василий", "Васильев",
+                                "vasiliev@mail.ru", 79933314644))
+            print("Добавлен клиент id: ",
+                  insert_client(curs, "Павел", "Павлов",
+                                "pavlov@mail.ru", 79913312643))
+            print("Добавлена клиент id: ",
+                  insert_client(curs, "Николай", "Николаев",
+                                "nikolayev@mail.ru"))
+            print("Данные в таблицах")
+            curs.execute("""
+                SELECT c.id, c.name, c.lastname, c.email, p.number FROM clients c
+                LEFT JOIN phonenumbers p ON c.id = p.client_id
+                ORDER by c.id
+                """)
+            pprint(curs.fetchall())
+            # 3. Добавляем клиенту номер телефона(одному первый, одному второй)
+            print("Телефон добавлен клиенту id: ",
+                  insert_tel(curs, 2, 79877876543))
+            print("Телефон добавлен клиенту id: ",
+                  insert_tel(curs, 1, 79621994802))
+
+            print("Данные в таблицах")
+            curs.execute("""
+                SELECT c.id, c.name, c.lastname, c.email, p.number FROM clients c
+                LEFT JOIN phonenumbers p ON c.id = p.client_id
+                ORDER by c.id
+                """)
+            pprint(curs.fetchall())
+            # 4. Изменим данные клиента
+            print("Изменены данные клиента id: ",
+                  update_client(curs, 4, "Федор", None, '123@mail.ru'))
+            # 5. Удаляем клиенту номер телефона
+            print("Телефон удалён c номером: ",
+                  delete_phone(curs, '79621994802'))
+            print("Данные в таблицах")
+            curs.execute("""
+                SELECT c.id, c.name, c.lastname, c.email, p.number FROM clients c
+                LEFT JOIN phonenumbers p ON c.id = p.client_id
+                ORDER by c.id
+                """)
+            pprint(curs.fetchall())
+            # 6. Удалим клиента номер 2
+            print("Клиент удалён с id: ",
+                  delete_client(curs, 2))
+            curs.execute("""
+                            SELECT c.id, c.name, c.lastname, c.email, p.number FROM clients c
+                            LEFT JOIN phonenumbers p ON c.id = p.client_id
+                            ORDER by c.id
+                            """)
+            pprint(curs.fetchall())
+            # 7. Найдём клиента
+            print('Найденный клиент по имени:')
+            pprint(find_client(curs, 'Василий'))
+
+            print('Найденный клиент по email:')
+            pprint(find_client(curs, None, None, 'ivanov@mail.ru'))
+
+            print('Найденный клиент по имени, фамилии и email:')
+            pprint(find_client(curs, 'Иван', 'Иванов',
+                               'ivanov@mail.ru'))
+
+            print('Найденный клиент по имени, фамилии, телефону и email:')
+            pprint(find_client(curs, 'Петр', 'Петров',
+                               'petrov@mail.ru', '79993318644'))
+
+            print('Найденный клиент по имени, фамилии, телефону:')
+            pprint(find_client(curs, None, None, None, '79913312643'))
